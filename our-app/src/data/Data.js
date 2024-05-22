5c651a9ab5c35966a02b99e76212da22b77b5240
unknown <dejvidpapa@gmail.com>
2024-05-22 11:10:11 +0200

    Data added , filled stars

---
+++ our-app/src/data/Data.js
@@ -0,0 +1,301 @@
+import { IoStar } from "react-icons/io5";
+
+export const navbar = [
+  {
+    id: 1,
+    path: "/",
+    nav: "Home",
+  },
+  {
+    id: 2,
+    path: "/about",
+    nav: "About",
+  },
+  {
+    id: 3,
+    path: "/shop",
+    nav: "Shop",
+  },
+  {
+    id: 4,
+    path: "/cart",
+    nav: "Cart",
+  },
+  {
+    id: 5,
+    path: "/contact",
+    nav: "Contact",
+  },
+];
+
+export const banners = [
+  {
+    banner: "../images/slider3.jpg",
+  },
+  {
+    banner: "../images/slider2.jpg",
+  },
+  {
+    banner: "../images/slider1.jpg",
+  },
+];
+
+export const bannar_down = [
+  {
+    img: "../images/MEN.jpg",
+    name: "MEN",
+  },
+  {
+    img: "../images/WOMEN.jpg",
+    name: "WOMEN",
+  },
+  {
+    img: "../images/ACCESSORIES.jpg",
+    name: "ACCESSORIES",
+  },
+];
+
+export const feateures = [
+  {
+    id: 1,
+    img: "../images/pink1.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 2,
+    img: "../images/top_jeans_side.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Kids",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 3,
+    img: "../images/white_back.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Mens",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 4,
+    img: "../images/white_face.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 5,
+    img: "../images/top_jeans_side.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 6,
+    img: "../images/light_black_face.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+
+  {
+    id: 7,
+    img: "../images/red.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Kurti",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 8,
+    img: "../images/black_face.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Kurti",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 9,
+    img: "../images/black_side.jpg",
+    tag: "new",
+    btn: "Select Options",
+    category: "Womens",
+    title: "Kurti",
+    short_description: "Lorem ipsum dolor",
+    rating: Array(5).fill({ icon: <IoStar /> }),
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  
+];
+
+export const reviews = [
+  {
+    title: "Customer Review",
+    description:
+      "a spoken or written account of a person, object, or event.people who had seen him were able to give a description",
+    customer_img: "../images/testi1.jpg",
+    customer_name: "John Smith",
+    position: "Online Marketer",
+  },
+
+  {
+    title: "Customer Review",
+    description:
+      "a spoken or written account of a person, object, or event.people who had seen him were able to give a description",
+    customer_img: "../images/testi2.jpg",
+    customer_name: "John Smith",
+    position: "Online Marketer",
+  },
+
+  {
+    title: "Customer Review",
+    description:
+      "a spoken or written account of a person, object, or event.people who had seen him were able to give a description",
+    customer_img: "../images/testi3.jpg",
+    customer_name: "John Smith",
+    position: "Online Marketer",
+  },
+];
+
+export const blogs = [
+  {
+    id: 1,
+    img: "../images/10-home_default.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+  {
+    id: 2,
+    img: "../images/11-home_default.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+  {
+    id: 3,
+    img: "../images/12-home_default.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+  {
+    id: 4,
+    img: "../images/13-home_default.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+  {
+    id: 5,
+    img: "../images/kurti.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+  {
+    id: 6,
+    img: "../images/14-home_default.jpg",
+    tag: "8 May",
+    title: "Shop",
+    short_description: "Lorem ipsum dolor",
+    read_more: "Read More",
+  },
+];
+
+export const footer = [
+  {
+    id: 2,
+    header: "Contact Us",
+    content1: "About Us",
+    content2: "Legal Notices",
+    content3: "Addresses",
+    content4: "Order",
+    content5: "Payments",
+    content6: "Suppliers",
+  },
+  {
+    id: 3,
+    header: "Information",
+    content1: "About Us",
+    content2: "Legal Notices",
+    content3: "Addresses",
+    content4: "Order",
+    content5: "Payments",
+    content6: "Suppliers",
+  },
+  {
+    id: 4,
+    header: "My Account",
+    content1: "About Us",
+    content2: "Legal Notices",
+    content3: "Addresses",
+    content4: "Order",
+    content5: "Payments",
+    content6: "Suppliers",
+  },
+  {
+    id: 5,
+    header: "Company",
+    content1: "About Us",
+    content2: "Legal Notices",
+    content3: "Addresses",
+    content4: "Order",
+    content5: "Payments",
+    content6: "Suppliers",
+  },
+];
