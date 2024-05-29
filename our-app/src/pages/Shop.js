23b1b5cfc2c67965471c458203e019266acf4811
ErliJoanidhi <erlijoanidhi@gmail.com>
2024-05-29 08:50:31 +0200

    Added no search result again

--- our-app/src/pages/Shop.js
+++ our-app/src/pages/Shop.js
@@ -12,7 +12,6 @@ const Shop = () => {
   const { favorites, toggleFavorite } = useFavorites();
   const location = useLocation();
   const navigate = useNavigate();
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -37,15 +36,6 @@ const Shop = () => {
     }
 
     setFilteredProducts(filtered);
-
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 479);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
   }, [location.search]);
 
   const handleOpen = (productid) => {
@@ -90,117 +80,73 @@ const Shop = () => {
             Mens
           </button>
         </div>
-        {isMobile ? (
-          <div className="mobile">
-            {filteredProducts.map((val, key) => (
-              <div className="features flex gap-8 mt-8" key={key}>
-                <div className="overflow-hidden relative m-2">
-                  <div className="image-container relative">
-                    <img src={val.img} alt={val.category} className="w-full" />
-                    <div className="hidden absolute bottom-0 bg-red-700 opacity-65 hover:opacity-100 w-full text-center text-white pt-4 pb-4 transition-all">
-                      <div className="flex justify-center align-middle">
-                        <button className="text-2xl">
-                          <BiCart />
-                        </button>
-                        <button
-                          type="button"
-                          className="whitespace-nowrap uppercase"
-                          onClick={() => handleOpen(val.id)}
-                        >
-                          {val.btn}
-                        </button>
-                      </div>
-                    </div>
-                    <div className="tag absolute top-0 z-10">
-                      <p className="bg-green-600 m-2 rounded-full w-12 h-12 grid place-items-center text-white">
-                        {val.tag}
-                      </p>
-                    </div>
-                    <div
-                      className="absolute top-0 right-0 p-4 m-4 cursor-pointer"
-                      onClick={() => toggleFavorite(val.id)}
-                    >
-                      <BiHeart
-                        className={`text-2xl ${
-                          favorites.includes(val.id)
-                            ? "text-red-600"
-                            : "text-gray-500"
-                        }`}
-                      />
+        {filteredProducts.length > 0 ? (
+          <div className="grid grid-cols-4 gap-8">
+          {filteredProducts.map((val, key) => (
+            <div className="features flex gap-8 mt-8" key={key}>
+              <div className="overflow-hidden relative m-2">
+                <div className="image-container relative">
+                  <img src={val.img} alt={val.category} className="w-full" />
+                  <div className="hidden absolute bottom-0 bg-red-700 opacity-65 hover:opacity-100 w-full text-center text-white pt-4 pb-4 transition-all">
+                    <div className="flex justify-center align-middle">
+                      <button className="text-2xl">
+                        <BiCart />
+                      </button>
+                      <button
+                        type="button"
+                        className="whitespace-nowrap uppercase"
+                        onClick={() => handleOpen(val.id)}
+                      >
+                        {val.btn}
+                      </button>
                     </div>
                   </div>
-                  <div className="product-details text-center mt-2">
-                    <p className="mb-2">{val.short_description}</p>
-                    <p className="mb-2">{val.title}</p>
-                    <div className="flex justify-center mb-2 text-yellow-700">
-                      {val.rating &&
-                        val.rating.map((star, index) => (
-                          <p key={index}>{star.icon}</p>
-                        ))}
-                    </div>
-                    <p className="text-red-600">${val.price}</p>
+                  <div className="tag absolute top-0 z-10">
+                    <p className="bg-green-600 m-2 rounded-full w-12 h-12 grid place-items-center text-white">
+                      {val.tag}
+                    </p>
+                  </div>
+                  <div
+                    className="absolute top-0 right-0 p-4 m-4 cursor-pointer"
+                    onClick={() => toggleFavorite(val.id)}
+                  >
+                    <BiHeart
+                      className={`text-2xl ${
+                        favorites.includes(val.id)
+                          ? "text-red-600"
+                          : "text-gray-500"
+                      }`}
+                    />
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="grid grid-cols-4 gap-8 desktop">
-            {filteredProducts.map((val, key) => (
-              <div className="features flex gap-8 mt-8" key={key}>
-                <div className="overflow-hidden relative m-2">
-                  <div className="image-container relative">
-                    <img src={val.img} alt={val.category} className="w-full" />
-                    <div className="hidden absolute bottom-0 bg-red-700 opacity-65 hover:opacity-100 w-full text-center text-white pt-4 pb-4 transition-all">
-                      <div className="flex justify-center align-middle">
-                        <button className="text-2xl">
-                          <BiCart />
-                        </button>
-                        <button
-                          type="button"
-                          className="whitespace-nowrap uppercase"
-                          onClick={() => handleOpen(val.id)}
-                        >
-                          {val.btn}
-                        </button>
-                      </div>
-                    </div>
-                    <div className="tag absolute top-0 z-10">
-                      <p className="bg-green-600 m-2 rounded-full w-12 h-12 grid place-items-center text-white">
-                        {val.tag}
-                      </p>
-                    </div>
-                    <div
-                      className="absolute top-0 right-0 p-4 m-4 cursor-pointer"
-                      onClick={() => toggleFavorite(val.id)}
-                    >
-                      <BiHeart
-                        className={`text-2xl ${
-                          favorites.includes(val.id)
-                            ? "text-red-600"
-                            : "text-gray-500"
-                        }`}
-                      />
-                    </div>
+                <div className="product-details text-center mt-2">
+                  <p className="mb-2">{val.short_description}</p>
+                  <p className="mb-2">{val.title}</p>
+                  <div className="flex justify-center mb-2 text-yellow-700">
+                    {val.rating &&
+                      val.rating.map((star, index) => (
+                        <p key={index}>{star.icon}</p>
+                      ))}
                   </div>
-                  <div className="product-details text-center mt-2">
-                    <p className="mb-2">{val.short_description}</p>
-                    <p className="mb-2">{val.title}</p>
-                    <div className="flex justify-center mb-2 text-yellow-700">
-                      {val.rating &&
-                        val.rating.map((star, index) => (
-                          <p key={index}>{star.icon}</p>
-                        ))}
-                    </div>
-                    <p className="text-red-600">${val.price}</p>
-                  </div>
+                  <p className="text-red-600">${val.price}</p>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+          <div className="flex flex-col items-center justify-center m-10">
+            <p className="text-xl text-gray-600 mb-4">No products found.</p>
+            <img
+              src="../images/NothinHere.png"
+              width={400}
+              height={400}
+              alt="nothing"
+            />
           </div>
         )}
       </div>
-
+        
       <Model
         isModalOpen={isModalOpen !== null}
         data={feateures.find((feature) => feature.id === isModalOpen)}
